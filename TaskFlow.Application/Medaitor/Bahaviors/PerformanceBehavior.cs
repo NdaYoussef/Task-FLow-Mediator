e74642859e32b31112249f7974c508416b64e555
NdaMousa <ndayou99@gmail.com>
2026-07-12 11:07:45 +0300

    add validator behavior

---
+++ TaskFlow.Application/Medaitor/Bahaviors/PerformanceBehavior.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFlow.Application.Medaitor.Bahaviors
+{
+    internal class PerformanceBehavior
+    {
+    }
+}
---
+++ TaskFlow.Application/Medaitor/Bahaviors/ValidationBehavior.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFlow.Application.Medaitor.Bahaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var validationResults = _validators
+                    .Select(v => v.Validate(context))
+                    .SelectMany(result => result.Errors)
+                    .Where(failure => failure != null)
+                    .ToList();
+                if (validationResults.Count != 0)
+                {
+                    throw new ValidationException(validationResults);
+                }
+            }
+            return next();
+        }
+    }
+}
