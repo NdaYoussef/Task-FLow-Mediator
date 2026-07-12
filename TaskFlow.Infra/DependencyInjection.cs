c18d3476536ce9c84c6203ed444713f5e7eaaa3f
NdaMousa <ndayou99@gmail.com>
2026-07-12 11:14:42 +0300

    add di class

--- TaskFlow.Infra/DependencyInjection.cs
+++ TaskFlow.Infra/DependencyInjection.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TaskFlow.Application.Medaitor.Bahaviors;
 
 namespace TaskFlow.Infra
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddApplication(
+       this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+
+                cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+
+            return services;
+        }
     }
 }
