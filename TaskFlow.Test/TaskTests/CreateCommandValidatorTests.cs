ab9fb2b7cd36d2861e09d35a73c68746526782cf
NdaMousa <ndayou99@gmail.com>
2026-08-23 19:16:55 +0300

    Add first XUnit test cases for Task Item Creation

---
+++ TaskFlow.Test/TaskTests/CreateCommandValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation.TestHelper;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static TaskFlow.Application.Features.Tasks.Commands.Create;
+
+namespace TaskFlow.Test.TaskTests
+{
+    //workflow 
+    /*
+     * 1- Valid Command 
+     * 2- Validation Error Command
+     * 3- DataPbase interaction error Command
+     * 
+     * */
+    public class CreateCommandValidatorTests 
+    {
+        private readonly Validator _validator = new Validator();
+
+
+        [Fact]
+        public void Should_Have_Error_When_Title_Is_Empty()
+        {
+            //arrange
+            var command = new Command("first title", "Valid Description");
+
+            //Act
+            var result = _validator.TestValidate(command);
+
+            //Assert
+            result.ShouldHaveValidationErrorFor(t => t.title).WithErrorMessage("Title is required");
+        }
+
+
+        [Fact]
+        public void Should_Have_Error_When_Description_Is_Empty()
+        {
+            var command = new Command("title valid", "");
+
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(d => d.description).WithErrorMessage("Description is required");
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Command_Is_Valid()
+        {
+            var command = new Command("Valid Title"," Valid Description");
+            var result = _validator.TestValidate(command);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
