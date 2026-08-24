6a3f1c0a90486e9b069d10293a986989c43829fe
NdaMousa <ndayou99@gmail.com>
2026-08-24 17:37:39 +0300

    remove nuint packages and let only XUnit

--- TaskFlow.Test/TaskTests/CreateCommandValidatorTests.cs
+++ TaskFlow.Test/TaskTests/CreateCommandValidatorTests.cs
@@ -12,7 +12,7 @@ namespace TaskFlow.Test.TaskTests
     /*
      * 1- Valid Command 
      * 2- Validation Error Command
-     * 3- DataPbase interaction error Command
+     * 3- Database interaction error Command
      * 
      * */
     public class CreateCommandValidatorTests 
@@ -21,10 +21,11 @@ namespace TaskFlow.Test.TaskTests
 
 
         [Fact]
+        
         public void Should_Have_Error_When_Title_Is_Empty()
         {
             //arrange
-            var command = new Command("first title", "Valid Description");
+            var command = new Command(string.Empty, "Valid Description");
 
             //Act
             var result = _validator.TestValidate(command);
@@ -34,15 +35,17 @@ namespace TaskFlow.Test.TaskTests
         }
 
 
+        
         [Fact]
         public void Should_Have_Error_When_Description_Is_Empty()
         {
             var command = new Command("title valid", "");
 
             var result = _validator.TestValidate(command);
-            result.ShouldHaveValidationErrorFor(d => d.description).WithErrorMessage("Description is required");
+            result.ShouldHaveValidationErrorFor(d => d.description).WithErrorMessage("Description is required.");
         }
 
+        
         [Fact]
         public void Should_Not_Have_Error_When_Command_Is_Valid()
         {
