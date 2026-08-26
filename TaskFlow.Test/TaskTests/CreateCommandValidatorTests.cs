1b2a135919f0c0d420e6b3962ae93cd83bc97fe0
NdaMousa <ndayou99@gmail.com>
2026-08-26 15:20:52 +0300

    use theory instead of 3 methods facts

--- TaskFlow.Test/TaskTests/CreateCommandValidatorTests.cs
+++ TaskFlow.Test/TaskTests/CreateCommandValidatorTests.cs
@@ -56,36 +56,41 @@ namespace TaskFlow.Test.TaskTests
         }
 
 
-        [Fact]
-        
-        public void Should_Have_Error_When_Title_Is_Empty()
+        #region Test Using Fact
+        //[Fact]
+
+        //public void Should_Have_Error_When_Title_Is_Empty()
+        //{
+        //    //arrange
+        //    var command = new Command(string.Empty, "Valid Description");
+
+        //    //Act
+        //    var result = _validator.TestValidate(command);
+
+        //    //Assert
+        //    result.ShouldHaveValidationErrorFor(t => t.title).WithErrorMessage("Title is required");
+        //}
+
+
+
+        //[Fact]
+        //public void Should_Have_Error_When_Description_Is_Empty()
+        //{
+        //    var command = new Command("title valid", "");
+
+        //    var result = _validator.TestValidate(command);
+        //    result.ShouldHaveValidationErrorFor(d => d.description).WithErrorMessage("Description is required.");
+        //} 
+        #endregion
+
+
+        [Theory]
+        [InlineData("", "Valid Description")]
+        [InlineData("Valid Title", "")]
+        [InlineData("Valid Title", "Valid Description")]
+        public void Should_Not_Have_Error_When_Command_Is_Valid(string Title , string Description)
         {
-            //arrange
-            var command = new Command(string.Empty, "Valid Description");
-
-            //Act
-            var result = _validator.TestValidate(command);
-
-            //Assert
-            result.ShouldHaveValidationErrorFor(t => t.title).WithErrorMessage("Title is required");
-        }
-
-
-        
-        [Fact]
-        public void Should_Have_Error_When_Description_Is_Empty()
-        {
-            var command = new Command("title valid", "");
-
-            var result = _validator.TestValidate(command);
-            result.ShouldHaveValidationErrorFor(d => d.description).WithErrorMessage("Description is required.");
-        }
-
-        
-        [Fact]
-        public void Should_Not_Have_Error_When_Command_Is_Valid()
-        {
-            var command = new Command("Valid Title"," Valid Description");
+            var command = new Command(Title, Description);
             var result = _validator.TestValidate(command);
             result.ShouldNotHaveAnyValidationErrors();
         }
